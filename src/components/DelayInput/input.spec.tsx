892ea947268e9887c7dfb214e3ef33946c080a5c
김원태 <rladnjsxo123@naver.com>
2025-01-22 20:13:07 +0900

    refactor: 타이머 비동기 테스트

--- src/components/DelayInput/input.spec.tsx
+++ src/components/DelayInput/input.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, RenderResult, screen } from "@testing-library/react";
+import { act, fireEvent, render, RenderResult, screen } from "@testing-library/react";
 import { DelayInput } from ".";
 import '@testing-library/jest-dom';
 
@@ -7,12 +7,14 @@ describe('DelayInput', () => {
     let renderResult: RenderResult
     let handleChange: jest.Mock
     beforeEach(() => {
+        jest.useFakeTimers()
         handleChange = jest.fn()
         renderResult = render(<DelayInput onChange={handleChange} />)
     })
 
     afterEach(() => {
         renderResult.unmount()
+        jest.useFakeTimers()
     })
 
     it('shoud display empty text on initial render', () => {
@@ -29,5 +31,18 @@ describe('DelayInput', () => {
         const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
         expect(spanNode).toHaveTextContent('입력 중...')
     });
+
+    it('shoud display "입력한 텍스트: Test Input Text" after 1 second', async () => {
+        const inputText = 'Test Input Text'
+        const inputNode = screen.getByTestId('input-text') as HTMLInputElement
+        fireEvent.change(inputNode, { target: { value: inputText } })
+
+        await act(() => {
+            jest.runAllTimers()
+        })
+
+        const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
+        expect(spanNode).toHaveTextContent(`입력한 텍스트: ${inputText}`)
+    })
 
 });
